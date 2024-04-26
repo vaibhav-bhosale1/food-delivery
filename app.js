33095c723ded3f3c16d79a0d0cb5f103713ecf84
Vaibhav Shivaji Bhosale <textmevaibhav@gmail.com>
2024-04-26 20:06:03 +0530

    initial

---
+++ app.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const app=express();
+const ejs=require('ejs');
+const path=require("path");
+const expresslayout=require('express-ejs-layouts')
+const PORT=process.env.PORT || 3000
+//ejs 
+
+app.use(express.static(path.resolve('./public')))
+app.set("view engine",'ejs');
+app.set("views",path.resolve("./resources/views"));
+
+
+
+
+
+//route
+app.get('/',(req,res)=>{
+   res.render("home");
+})
+
+app.listen(PORT,()=>{console.log(`listening on port:${PORT}`)})
